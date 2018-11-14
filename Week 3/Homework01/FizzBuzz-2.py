622d4dd81a76531e11719c28ba49cb3671d8d008
YuliyaKoldayeva <yuliya.koldayeva@gmail.com>
2018-11-13 20:56:24 -0500

    Second solution for FizzBuzz in Python

---
+++ Week 3/Homework01/FizzBuzz-2.py
@@ -0,0 +1,8 @@
+word = ""
+for number in range(26):
+    word = str(number)+": "
+    if number%3 == 0:
+        word = word+"Fizz"
+    if number%5 == 0:
+        word=word + "Buzz"
+    print(word)
