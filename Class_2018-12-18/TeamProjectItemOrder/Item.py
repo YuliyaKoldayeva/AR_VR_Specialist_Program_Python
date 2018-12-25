61fd7b458d6b021a1391c10ecda51947fe6e8dba
YuliyaKoldayeva <yuliya.koldayeva@gmail.com>
2018-12-25 14:17:06 -0500

    Team Project Order - Independent Classes called

--- Class_2018-12-18/TeamProjectItemOrder/Item.py
+++ Class_2018-12-18/TeamProjectItemOrder/Item.py
@@ -1,34 +1,45 @@
 class Item:
+    __item_name_field_length = 25
+    __price_field_length = 10
 
-
-    def __init__(self, sku, name, price, taxable):
+    def __init__(self, sku: int, name: str, price: float, taxable: bool):
         """Instantiating the items"""
         self.__sku = sku
         self.__name = name
         self.__price = price
         self.__taxable = taxable
-        self.__first_field_length = 15
-        self.__second_field_length = 5
+
+    @staticmethod
+    def get_price_field_length(self):
+        """Static method to refer the length of the price field"""
+        return Item.__price_field_length
+
+    @staticmethod
+    def get_item_name_field_length(self):
+        """Static method to refer the length of the item name field"""
+        return Item.__item_name_field_length
 
 
     def print_item(self):
-        """Printing the description of the item"""
-        self.item_description=("| {} | {} {} | $ {:0,.2f}{} | {} |".format(self.__sku,
-                                            self.__name,
-                                            ("."*(self.__first_field_length-len(self.__name))),
-                                            self.__price,
-                                            (" "*(self.__second_field_length-len(str(self.__price)))),
-                                            ("T"*(self.__taxable)+ " "*(1-self.__taxable))))
-        return self.item_description
+        """Returns the formatted description of the item"""
+        self.__name_field_fill = "." * (Item.__item_name_field_length - len(self.__name))
+        self.__price_field_fill = "." * (Item.__price_field_length - len(str(self.__price)))
+        self.__taxable_field_fill = "T" * self.__taxable + " " * (1 - self.__taxable)
 
+        self.__item_description = ("| {} | {} {} | $ {}{:0,.2f} |  {}  |".format(self.__sku,
+                                                                                 self.__name,
+                                                                                 self.__name_field_fill,
+                                                                                 self.__price_field_fill,
+                                                                                 self.__price,
+                                                                                 self.__taxable_field_fill))
+        return self.__item_description
 
     def item_base_price(self):
         """Return gets the price and makes it available to user"""
         return self.__price
 
-
     def item_gst_amount(self):
-        """Returns tax amount"""
+        """Returns GST tax amount"""
         if self.__taxable:
             self.__gst_amount = self.__price * .05
         else:
@@ -36,7 +47,7 @@ class Item:
         return self.__gst_amount
 
     def item_pst_amount(self):
-        """Returns tax amount"""
+        """Returns PST tax amount"""
         if self.__taxable:
             self.__pst_amount = self.__price * .09975
         else:
@@ -44,28 +55,27 @@ class Item:
         return self.__pst_amount
 
     def get_item_total(self):
+        """Returns the TOTAL amount to pay, Taxes included"""
         self.__item_total = Item.item_base_price(self) + Item.item_pst_amount(self) + Item.item_pst_amount(self)
         return self.__item_total
 
-    def get_item_sku(self):
-        return self.__sku
+# Test area
 
-
-a1 = Item(12345678, "first item", 2.00, True)# instantiating the first item
-a2 = Item(23456789, "second item", 3.00, False)# instantiating the second item
-a3 = Item(34567891, "third item", 10.00, True)# instantiating the third item
-
-print(a1.print_item())
-print(a2.print_item())
-print(a1.item_base_price())
-print(a1.item_gst_amount())
-print(a1.item_pst_amount())
-print(a2.item_base_price())
-print(a2.item_gst_amount())
-print(a2.item_pst_amount())
-print(a1.get_item_total())
-print(a2.get_item_total())
-print(a3.get_item_total())
+# a1 = Item(12345678, "first item", 2.00, True)# instantiating the first item
+# a2 = Item(23456789, "second item", 3.00, False)# instantiating the second item
+# a3 = Item(34567891, "third item", 10.00, True)# instantiating the third item
+#
+# print(a1.print_item())
+# print(a2.print_item())
+# print(a1.item_base_price())
+# print(a1.item_gst_amount())
+# print(a1.item_pst_amount())
+# print(a2.item_base_price())
+# print(a2.item_gst_amount())
+# print(a2.item_pst_amount())
+# print(a1.get_item_total())
+# print(a2.get_item_total())
+# print(a3.get_item_total())
 
 
 
