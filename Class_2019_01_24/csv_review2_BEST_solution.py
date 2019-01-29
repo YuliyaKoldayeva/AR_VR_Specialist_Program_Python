03da85b17039356d0388f4a4faa20d4cf9831198
YuliyaKoldayeva <yuliya.koldayeva@gmail.com>
2019-01-28 21:48:18 -0500

    comments added

--- Class_2019_01_24/csv_review2_BEST_solution.py
+++ Class_2019_01_24/csv_review2_BEST_solution.py
@@ -12,18 +12,19 @@ with open('biostats.csv', 'rt') as my_csv_file:
         writer = csv.writer(my_csv_file, dialect='myDialect')
 
         for row in reader:
+            """Change ONLY entries in the weight and height columns            """
 
             try:
+                "Change the values from imperial to metric system"
                 row[3] = int(int(row[3]) * inch_to_cm)
                 row[4] = int(int(row[4]) * lb_to_kg)
 
             except:
+                "Head row will be catch by this exception"
                 if row[0] == "Name":
                     row[3] = "Height(cm)"
                     row[4] = "Weight(kg)"
                 else:
                     print("Data is not numeric")
 
-            new_line = row
-
-            writer.writerow(new_line)
+            writer.writerow(row)
