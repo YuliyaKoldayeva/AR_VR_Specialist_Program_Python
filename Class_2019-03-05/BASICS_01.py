10e4ce8e04a26a03ca4dbaee3a837eb8f6299396
YuliyaKoldayeva <yuliya.koldayeva@gmail.com>
2019-03-05 23:01:41 -0500

    HomeWork, Basics 01 - SOLUTION

--- Class_2019-03-05/BASICS_01.py
+++ Class_2019-03-05/BASICS_01.py
@@ -1,49 +1,68 @@
 import re
 
-input_list = [6, "g", 5, "b", "$", "c", 7, 26, 5, "z", "!"]
+
+def is_numeric(test_string):
+    pattern = '^[0-9]+$'
+    return re.match(pattern, test_string)
 
 
-def check_if_number(element):
+def is_character(test_string):
+    pattern = '^[a-zA-Z]{1}$'
+    return re.match(pattern, test_string)
 
-    if re.match("^[0-9]+$", element):
-        return True
+
+def is_special_character(test_string):
+    pattern = '^[\!\@\#\$\%\^\&\*\(\)]{1}$'
+    return re.match(pattern, test_string)
+
+def is_even_number(element):
+    if element%2==0:
+        return "This is an even number. "
     else:
-        return False
+        return ""
 
 
-
-print(check_if_number(5))
-print(check_if_number("a"))
-
-"""
-
-
-def find_max_value(input_list):
-    list_of_numbers = []
+def numeric_list_max(input_list):
+    only_numbers = []
     for element in input_list:
-        if check_if_number(element):
-            list_of_numbers.append(element)
+        test_string = str(element)
+        if is_numeric(test_string):
+            only_numbers.append(element)
+    return max(only_numbers)
 
-    return max(list_of_numbers)
 
-
-
-def print_the_statement(element,index):
-
-    statement = ("Element #{}".format(index))
-
-    return statement
-
+def current_vs_max(input_list, element):
+    if numeric_list_max(input_list) == element:
+        return "This is the highest number in the list."
+    else:
+        return "This is NOT the highest number in the list."
 
 
 
 def go_through_the_list(input_list):
-
+    counter = 0
+    character_set=set()
     for element in input_list:
-        check_if_number(element)
+        counter +=1
+        test_string = str(element)
+        print("Element #{}: Value is {}. ".format(counter, element), end="")
+
+        if is_numeric(test_string):
+            print("This is a number. " + is_even_number(element) + current_vs_max(input_list, element))
+
+        elif is_character(test_string):
+            character_set.add(test_string)
+            print("This is a character. So far, the characters found are", sorted(character_set))
+
+        elif is_special_character(test_string):
+            print("This is a special character")
+
+        else:
+            print("This is an element without clear classification")
 
 
 
+input_list = [6, "g", 5, "b", "$", "c", 7, 26, 5, "z", "!", "p6"]
 
-print(go_through_the_list(input_list))
-"""
+go_through_the_list(input_list)
+
