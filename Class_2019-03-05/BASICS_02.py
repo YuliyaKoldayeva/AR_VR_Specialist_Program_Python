a1578b0a3e8a0c5bca7a168ee9977bb75c153c4a
YuliyaKoldayeva <yuliya.koldayeva@gmail.com>
2019-03-05 23:46:49 -0500

    Basics02 - SOLVED

--- Class_2019-03-05/BASICS_02.py
+++ Class_2019-03-05/BASICS_02.py
@@ -1,24 +1,31 @@
-
-
 def create_list(min_value, max_value):
-
     list_of_numbers = list(range(min_value, max_value+1))
-
     return list_of_numbers
 
 
 def check_if_prime(element):
 
-    number_is_prime = True
-
-    if element != 1:
-
+    if element > 2:
         for each in range(2, element):
-            if element%each == 0:
-                return False
+            if (element%each) == 0:
+                return "This is NOT a prime number"
+        return "This is a PRIME NUMBER"
+    elif element == 2:
+        return "This is a prime number"
+    elif element == 1:
+        return "This is NOT a prime number"
 
 
-print("My list is", create_list(1,100))
+def check_the_elements(my_list):
+    for each in my_list:
+        print(each, check_if_prime(each))
+
+
+my_list = create_list(1,100)
+print("Creating my list of numbers from 1 to 100", my_list)
+
+check_the_elements(my_list)
+
 
 
 
